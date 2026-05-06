b2180927d85154c10ce8762956475a1526e6fe9d
Md-Rizuan <mdrizuanmozquree@gmail.com>
2026-05-06 19:56:32 +0600

    create new db form and save it in db

--- core/urls.py
+++ core/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from main.views import home
+from main.views import home, contact
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
+    path("contact", contact, name="contact"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
+from .models import Contact 
 
 def home(request):
     return render(request, 'home.html')
+
+
+def contact(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        subject = request.POST.get("subject")
+        message = request.POST.get("message")
+
+        Contact.objects.create(name=name, email=email, subject=subject, message=message)
+        
+
+    return render(request, "contact.html")
